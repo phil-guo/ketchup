9a2dbbcbacfee698aea631de1f4b72dbe47c2922
guor <1228115857@qq.com>
2020-03-30 18:01:16 +0800

    提交

--- src/Ketchup.Core/Modules/KernelModuleProvider.cs
+++ src/Ketchup.Core/Modules/KernelModuleProvider.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.Extensions.Logging;
 
 namespace Ketchup.Core.Modules
 {
@@ -8,17 +9,17 @@ namespace Ketchup.Core.Modules
     {
         private readonly List<KernelModule> _modules;
         private readonly KetchupPlatformContainer _container;
+        private readonly ILogger<KernelModuleProvider> _logger;
 
-        public KernelModuleProvider(List<KernelModule> modules, KetchupPlatformContainer container)
+        public KernelModuleProvider(List<KernelModule> modules, KetchupPlatformContainer container, ILogger<KernelModuleProvider> logger)
         {
             _modules = modules;
             _container = container;
+            _logger = logger;
         }
 
-        public List<KernelModule> Modules
-        {
-            get => _modules;
-        }
+        public List<KernelModule> Modules => _modules;
+
         public void Initialize()
         {
             _modules.ForEach(item =>
@@ -29,6 +30,7 @@ namespace Ketchup.Core.Modules
                 }
                 catch (Exception e)
                 {
+                    _logger.Log(LogLevel.Error, e, "initialize exception ...");
                 }
             });
         }
