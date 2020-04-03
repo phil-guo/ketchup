19e30126431e0415aaeb4e57acf3dde1bbd24cf9
simple <1228115857@qq.com>
2020-04-03 17:21:48 +0800

    添加sample项目

---
+++ sample/Ketchup.Sample.Server/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ketchup.Sample.Server
+{
+    public class Startup
+    {
+        public Startup() { }
+
+        public void ConfigureServices(IServiceCollection services) { }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) { }
+    }
+}
