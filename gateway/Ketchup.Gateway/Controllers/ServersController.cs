d8d94bb9c25350c01b5b3fcdf5b97dd4758a8d05
simple <1228115857@qq.com>
2020-10-29 14:51:39 +0800

    在面板上添加个了个简单的服务参数可视化功能

--- gateway/Ketchup.Gateway/Controllers/ServersController.cs
+++ gateway/Ketchup.Gateway/Controllers/ServersController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 using Ketchup.Consul.Internal.ConsulProvider;
 using Ketchup.Gateway.Internal;
 using Ketchup.Gateway.Internal.Filter;
@@ -35,6 +36,7 @@ namespace Ketchup.Gateway.Controllers
         [KetchupExceptionFilter]
         public async Task<object> GetAllServerEntry(string server, string service)
         {
+            var nodes = GetAllXmlDocNodes();
             var entries = await _server.GetAllServerEntry(server, service);
             foreach (var item in entries)
             {
@@ -43,20 +45,60 @@ namespace Ketchup.Gateway.Controllers
                 if (entry == null)
                     continue;
 
-                var dictionary = new Dictionary<string, string>();
+                var dictionary = new Dictionary<string, Dictionary<string, string>>
+                {
+                    {"Request", new Dictionary<string, string>()},
+                    {"Response", new Dictionary<string, string>()}
+                };
+
 
                 entry.InputType.ClrType.GetProperties().ToList().ForEach(property =>
                 {
                     if (property.Name == "Descriptor" || property.Name == "Parser")
                         return;
-                    dictionary.Add(property.Name?.Substring(0, 1).ToLower() + property.Name?.Substring(1),
-                        property.PropertyType.ToString());
+
+                    var nodeSummary = FindXmlDocNode(nodes, $"P:{entry.InputType.FullName}.{property.Name}");
+
+                    dictionary["Request"].Add(property.Name?.Substring(0, 1).ToLower() + property.Name?.Substring(1),
+                        property.PropertyType.ToString().Contains("Google.Protobuf.Collections.RepeatedField")
+                            ? $"List<> //{nodeSummary}"
+                            : $"{property.PropertyType.ToString()} //{nodeSummary}");
                 });
+
+                entry.OutputType.ClrType.GetProperties().ToList().ForEach(property =>
+                {
+                    if (property.Name == "Descriptor" || property.Name == "Parser")
+                        return;
+
+                    var nodeSummary = FindXmlDocNode(nodes, $"P:{entry.InputType.FullName}.{property.Name}");
+
+                    dictionary["Response"].Add(property.Name?.Substring(0, 1).ToLower() + property.Name?.Substring(1),
+                        property.PropertyType.ToString().Contains("Google.Protobuf.Collections.RepeatedField")
+                            ? $"List<> //{nodeSummary}"
+                            : $"{property.PropertyType.ToString()} //{nodeSummary}");
+                });
+
 
                 item.Parameter = JsonConvert.SerializeObject(dictionary);
             }
 
             return new KetchupResponse(entries);
+        }
+
+        private string FindXmlDocNode(IEnumerable<XElement> nodes, string member)
+        {
+            var node = nodes?.FirstOrDefault(item => item.FirstAttribute.Value.ToLower() == member.ToLower());
+
+            if (node == null)
+                return "";
+            return node.Descendants("summary").FirstOrDefault()?.Value.Replace("/n", "").Trim();
+        }
+
+        private IEnumerable<XElement> GetAllXmlDocNodes()
+        {
+            var xml = XDocument.Load("Ketchup.Gateway.xml");
+            var nodes = xml.Root?.Descendants("member");
+            return nodes;
         }
     }
 }
